57de9f9d334a275d04737aef603b542aa085aaf6
zlita <luca.jourdan@protonmail.com>
2022-12-09 19:09:09 +0100

    allow to easilly test the decision tree

--- decision_tree/decision_tree_learning.py
+++ decision_tree/decision_tree_learning.py
@@ -1,106 +1,158 @@
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
+import json
+
+
+class DT:
+
+    def __init__(self, max_depth=5, criterion="gini", weights=[1,1,1], max_features=None, max_leaf_nodes=None, spliter='best'):
+        self.max_depth =max_depth
+        self.criterion =criterion
+        self.weigths = {0 : weights[0], 1 : weights[1], 2 : weights[2]}
+        self.max_features = max_features
+        self.max_leaf_nodes = max_leaf_nodes
+        self.spliter = spliter
+
+    def load_config(self, json_file):
+        f = open(json_file)
+        data = json.load(f)
+
+        self.max_depth = data['max_depth']
+        self.criterion = data['criterion']
+        weights = data['weigths']
+        self.weigths = {0 : weights[0], 1 : weights[1], 2 : weights[2]}
+        self.spliter = data['spliter']
+
+        if data['max_features'] == "None":
+            self.max_features = None
+        else :
+            self.max_features = data['max_features']
+
+        if data['max_leaf_nodes'] == "None":
+            self.max_leaf_nodes = None
+        else :
+            self.max_leaf_nodes = data['max_leaf_nodes']
+
+        self.load_data(data["dataset"])
+
+
+    def load_data(self, csv_file):
+        data = pd.read_csv(csv_file)
+        data = data.dropna()
+        self.Y = data.iloc[:, -1].to_numpy(dtype=np.int8)
+        self.X = data.iloc[:, :-1].to_numpy(dtype=np.int8)
+
+
+    def learn_tree(self, X=None, Y=None):
+        if X is None:
+            X = self.X
+            Y = self.Y
+
+        self.clf = DecisionTreeClassifier(random_state=0, max_depth=self.max_depth, criterion=self.criterion,
+                                    max_features=self.max_features, max_leaf_nodes=self.max_leaf_nodes,
+                                    splitter=self.spliter, class_weight=self.weigths)
+
+        self.clf.fit(X, Y)
+
+
+    def test_tree(self, test_X, test_Y):
+        """
+        test a decision tree on a test dataset.
+        clf : a decision tree
+        test_X : decision tree's test input
+        test_Y : labels
+
+        return :
+        """
+
+        predictions = self.clf.predict(test_X)
+
+        True_0 =0
+        True_1 = 0
+        True_2 = 0
+        one_become_zero = 0
+        one_become_two = 0
+        zero_become_one = 0
+        zero_become_two = 0
+        two_become_zero = 0
+        two_become_one = 0
+        error_impact_secu = 0
+        error_impact_perf = 0
+
+        for pred, true in zip(predictions, test_Y):
+            if pred == 0 and true == 0:
+                True_0 += 1
+            elif pred == 1 and true == 1:
+                True_1 += 1
+            elif pred == 2 and true == 2:
+                True_2 += 1
+
+            elif pred == 0 and true == 1:
+                one_become_zero += 1
+            elif pred == 2 and true == 1:
+                one_become_two += 1
+            elif pred == 1 and true == 0:
+                zero_become_one += 1
+            elif pred == 2 and true == 0:
+                zero_become_two += 1
+            elif pred == 0 and true == 2:
+                two_become_zero += 1
+            elif pred == 1 and true == 2:
+                two_become_one += 1
+
+            if pred < true :
+                error_impact_perf += 1
+            if pred > true:
+                error_impact_secu += 1
+
+
+        good = True_0 + True_1 + True_2
+
+        return [good, error_impact_secu, error_impact_perf, True_0, True_1, True_2,
+                one_become_zero, one_become_two, zero_become_one, zero_become_two,
+                two_become_zero, two_become_one]
+
+
+    def compute_action(self, obs, train_coord, train_speed):
+        min_action = 2
+
+        for i in range(obs.num_obstacle):
+            print(train_coord)
+            action = self.clf.predict([[obs.coord[i][1], obs.coord[i][0] - float(train_coord[0]), train_speed]])
+            min_action = min(min_action, action)
+
+        return min_action
 
 
 
-def learn_tree(X, Y, max_depth=5, criterion="gini", weights=[1,1,1], max_features=None, max_leaf_nodes=None, spliter='best'):
 
-    clf = DecisionTreeClassifier(random_state=0, max_depth=max_depth, criterion=criterion,
-                                max_features=max_features, max_leaf_nodes=max_leaf_nodes,
-                                splitter=spliter, class_weight={0 : weights[0], 1 : weights[1]})
+if __name__ == '__main__':
 
-    clf.fit(X, Y)
-    return clf
+    csv_file = "dataset.csv"
 
+    with open(csv_file) as f:
+        nbr_line = sum(1 for line in f)
 
+    data = pd.read_csv(csv_file, nrows=int(nbr_line*0.8))
+    data = data.dropna()
+    Y = data.iloc[:, -1].to_numpy(dtype=np.int8)
+    X = data.iloc[:, :-1].to_numpy(dtype=np.int8)
 
-def test_tree(clf,test_X, test_Y):
-    """
-    test a decision tree on a test dataset.
-    clf : a decision tree
-    test_X : decision tree's test input
-    test_Y : labels
+    data = None
 
-    return :
-    """
+    data = pd.read_csv(csv_file, skiprows = range(1, int(nbr_line*0.8)), nrows=int(nbr_line*0.2))
+    data = data.dropna()
+    test_Y = data.iloc[:, -1].to_numpy(dtype=np.int8)
 
-    predictions = clf.predict(test_X)
+    sata = None
+    test_X = data.iloc[:, :-1].to_numpy()
 
-    True_0 =0
-    True_1 = 0
-    True_2 = 0
-    one_become_zero = 0
-    one_become_two = 0
-    zero_become_one = 0
-    zero_become_two = 0
-    two_become_zero = 0
-    two_become_one = 0
-    error_impact_secu = 0
-    error_impact_perf = 0
-
-    for pred, true in zip(predictions, test_Y):
-        if pred == 0 and true == 0:
-            True_0 += 1
-        elif pred == 1 and true == 1:
-            True_1 += 1
-        elif pred == 2 and true == 2:
-            True_2 += 1
-
-        elif pred == 0 and true == 1:
-            one_become_zero += 1
-        elif pred == 2 and true == 1:
-            one_become_two += 1
-        elif pred == 1 and true == 0:
-            zero_become_one += 1
-        elif pred == 2 and true == 0:
-            zero_become_two += 1
-        elif pred == 0 and true == 2:
-            two_become_zero += 1
-        elif pred == 1 and true == 2:
-            two_become_one += 1
-
-        if pred < true :
-            error_impact_perf += 1
-        if pred > true:
-            error_impact_secu += 1
+    i = 0
+    results = []
 
 
-    good = True_0 + True_1 + True_2
+    dt = DT()
 
-    return [good, error_impact_secu, error_impact_perf, True_0, True_1, True_2,
-            one_become_zero, one_become_two, zero_become_one, zero_become_two,
-            two_become_zero, two_become_one]
-
-
-
-
-
-
-
-csv_file = "dataset.csv"
-
-with open(csv_file) as f:
-    nbr_line = sum(1 for line in f)
-
-data = pd.read_csv(csv_file, nrows=int(nbr_line*0.8))
-data = data.dropna()
-Y = data.iloc[:, -1].to_numpy(dtype=np.int8)
-X = data.iloc[:, :-1].to_numpy(dtype=np.int8)
-
-data = None
-
-data = pd.read_csv(csv_file, skiprows = range(1, int(nbr_line*0.8)), nrows=int(nbr_line*0.2))
-data = data.dropna()
-test_Y = data.iloc[:, -1].to_numpy(dtype=np.int8)
-
-sata = None
-test_X = data.iloc[:, :-1].to_numpy()
-
-i = 0
-results = []
-
-
-clf = learn_tree(X, Y, 5, "entropy", [2, 3], 0.6)
-
-test_tree(clf, test_X, test_Y)
+    dt.learn_tree(X, Y)
+    dt.test_tree(test_X, test_Y)
--- test_ray.py
+++ test_ray.py
@@ -19,11 +19,12 @@ import copy
 import glob
 import re
 import matplotlib.pyplot as plt
+import decision_tree.decision_tree_learning as dt
 
 
 
-def eval_agent(env_test, model, ep=5):
-    
+def eval_agent(env_test, model, ep=5, dt=False):
+
     hist = {"collision":[],"timeout":[], "speed_ratio":[], "col_speed":[], "reward":[], "n_step":[]}
     for i in range(ep):
         obs = env_test.reset()
@@ -34,18 +35,21 @@ def eval_agent(env_test, model, ep=5):
         if i%50==0:
             print(i)
         while not done:
-            n_step+=1
-            action = model.compute_action(obs)
+            n_step += 1
+            if not dt:
+                action = model.compute_action(obs)
+            else:
+                action = model.compute_action(env_test.obstacles, env_test.train.coord, env_test.train.speed)
             obs, rewards, done, info = env_test.step(action)
             cum_rew += rewards
             speed_ratio.append(info["speed_ratio"])
-            
+
             if done:
                 hist["collision"].append(int(info["collision"]))
                 hist["col_speed"].append(info["col_speed"])
-                hist["timeout"].append(int(info["timeout"]))   
+                hist["timeout"].append(int(info["timeout"]))
                 hist["speed_ratio"].append(np.mean(speed_ratio))
-                hist["reward"].append(cum_rew) 
+                hist["reward"].append(cum_rew)
                 hist["n_step"].append(n_step)
 
     return hist
@@ -72,17 +76,25 @@ if __name__ == '__main__':
     parser.add_argument('--checkpoint', type=str,
                          default="rllib_test/3obs_img/CNN_LSTM_aux/checkpoint_003600/checkpoint-3600",
                         help='Checkpoint to test. This script will infers from this path the related params.json used for training')
-    
+
     parser.add_argument('--show', action="store_true",
                          default=False,
                         help='Used to activate visualisation')
     parser.add_argument('--obs_num', type=int,
                          default=3,
                         help='Number of obstacles to use in the test')
-    
+
     parser.add_argument('--num_ep', type=int,
                          default=1000,
                         help='Number of episodes used to test the agent')
+
+    parser.add_argument('--decision_tree', type=bool,
+                     default=False,
+                    help='If true a decision tree will be used instead of the RL agent.')
+
+    parser.add_argument('--decision_tree_params', type=str,
+                     default="decision_tree/params.json",
+                    help='The decision tree\'s parameters. Only used if decision_tree is true.')
 
     args = parser.parse_args()
     print(args)
@@ -91,8 +103,8 @@ if __name__ == '__main__':
     file=open(config_ray_file)
     policy_config = json.load(file)
 
-    
-    
+
+
     policy_config["num_workers"] = 1
 
     f = policy_config["env_config"]["env"]["obs_builder"]
@@ -106,42 +118,48 @@ if __name__ == '__main__':
     c = c.split(".")[1][:-2]
     policy_config["callbacks"]=eval(c)
     policy_config['log_level']= 'ERROR'
-    
+
     s = policy_config["sample_collector"]
     s = s.split("'")[1]
-    
+
     policy_config["sample_collector"] = eval(s)
-    
+
     policy_config["env_config"]["render"]["render"] = args.show
     policy_config["env_config"]["obstacle"]["num_obstacle"] = args.obs_num
 
     policy_config["model"]["custom_model"] = eval(policy_config["model"]["custom_model"])
-    
-    test_agent = dqn.ApexTrainer(env=TrainEnv, config=policy_config)
-    test_agent.restore(args.checkpoint)
+
+    if not args.decision_tree :
+        test_agent = dqn.ApexTrainer(env=TrainEnv, config=policy_config)
+        test_agent.restore(args.checkpoint)
+    else :
+        test_agent = dt.DT()
+        test_agent.load_config(args.decision_tree_params)
+        test_agent.learn_tree()
+
     env_test = TrainEnv(policy_config["env_config"])
 
-    hist = eval_agent(env_test, test_agent, ep=args.num_ep)
+    hist = eval_agent(env_test, test_agent, ep=args.num_ep, dt=args.decision_tree)
     #print(hist)
-    
+
     print("collision mean:",np.mean(hist["collision"]))
     print("collision std:",np.std(hist["collision"]))
     print("collision min:",np.min(hist["collision"]))
     print("collision max:",np.max(hist["collision"]))
-    
-    
+
+
     print("timeout:",np.mean(hist["timeout"]))
-    
+
     print("speed_ratio mean:",np.mean(hist["speed_ratio"]))
     print("speed_ratio std:",np.std(hist["speed_ratio"]))
     print("speed_ratio min:",np.min(hist["speed_ratio"]))
     print("speed_ratio max:",np.max(hist["speed_ratio"]))
-    
+
     print("reward mean:",np.mean(hist["reward"]))
     print("reward std:",np.std(hist["reward"]))
     print("reward min:",np.min(hist["reward"]))
     print("reward max:",np.max(hist["reward"]))
-    
+
     col_speed =  [i for i in hist["col_speed"] if i is not None]
     print("col speed mean:",np.mean(col_speed))
     print("col speed std:",np.std(col_speed))
@@ -150,64 +168,52 @@ if __name__ == '__main__':
         print("col speed max:",np.max(col_speed))
     except:
         pass
-        
-        
+
+
     print("n step mean:", np.mean(hist["n_step"]))
     print("n step std:", np.std(hist["n_step"]))
     print("n step min:", np.min(hist["n_step"]))
     print("n step max:", np.max(hist["n_step"]))
-    
-    
+
+
     ################################################
     col_speed =  [i for i in hist["col_speed"] if i is not None]
-    
-    
+
+
     dict_res = {"collision mean":np.mean(hist["collision"]),
     "collision std":np.std(hist["collision"]),
 #        "collision min":np.min(hist["collision"]),
 #        "collision max":np.max(hist["collision"]),
     "collision":hist["collision"],
-    
+
     "timeout mean":np.mean(hist["timeout"]),
     "timeout":hist["timeout"],
-    
+
     "speed_ratio mean":np.mean(hist["speed_ratio"]),
     "speed_ratio std":np.std(hist["speed_ratio"]),
     "speed_ratio min":np.min(hist["speed_ratio"]),
     "speed_ratio max":np.max(hist["speed_ratio"]),
     "speed_ratio":hist["speed_ratio"],
-    
+
     "reward mean":np.mean(hist["reward"]),
     "reward std":np.std(hist["reward"]),
     "reward min":np.min(hist["reward"]),
     "reward max":np.max(hist["reward"]),
     "reward":hist["reward"],
-    
+
     "col speed mean":np.mean(col_speed),
     "col speed std":np.std(col_speed),
     "col speed min":np.min(col_speed) if col_speed else 0,
     "col speed max":np.max(col_speed) if col_speed else 0,
     "col_speed":hist["col_speed"],
-    
+
     "n step mean":np.mean(hist["n_step"]),
     "n step std":np.std(hist["n_step"]),
     "n step min":np.min(hist["n_step"]),
     "n step max":np.max(hist["n_step"]),
     "n step":hist["n_step"]}
-    
-    
+
+
     # name = "run5_1000ep_1obs_t"
     # with open(f'analysis/validation/{name}.json', 'w') as fp:
         # json.dump(dict_res, fp,  indent=4)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
